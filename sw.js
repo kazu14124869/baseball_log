11ee5bdf19ae69f0dd2be01eafe14215f850cb67
kazu14124869 <kazu120304@gmail.com>
2026-09-05 23:47:58 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  // キャッシュ制御が必要な場合はここに追加可能
+});
